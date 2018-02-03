93c92c60bec603ce26394a031815d23374826633
ElaineHuang <starbrave1313677@gmail.com>
2018-02-03 19:26:02 +0800

    update aquculture

---
+++ src/components/Aquaculture/locale.js
@@ -0,0 +1,32 @@
+const aquacuturistTitle = {
+  id: 'aquaculture.aquacuturist-title',
+  defaultMessage: 'Aquacuturist',
+};
+
+const coldFresh = {
+  id: 'aquaculture.cold-fresh',
+  defaultMessage: 'Cold Freshwater',
+};
+
+const coldSalt = {
+  id: 'aquaculture.cold-seawater',
+  defaultMessage: 'Cold Seawater',
+};
+
+const warmFresh = {
+  id: 'aquaculture.warm-fresh',
+  defaultMessage: 'Warm Freshwater',
+};
+
+const warmSalt = {
+  id: 'aquaculture.warm-seawater',
+  defaultMessage: 'Warn Seawater',
+};
+
+export default {
+  aquacuturistTitle,
+  coldFresh,
+  coldSalt,
+  warmFresh,
+  warmSalt,
+};
