ee165d0fcc165daaffd1a4dee1185a06162ad8a0
Ben Liu <ben196888@gmail.com>
2018-02-06 14:04:25 +1100

    Update index.js

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,33 +3,30 @@ import React, { PureComponent } from 'react';
 import styles from './button.scss';
 import cx from 'classnames';
 
-export default class Dialog extends PureComponent {
+const Dialog = ({ className, onClick, children, type }) => {
+  return (
+    <button
+      className={styles[className]}
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+};
 
-  static propTypes = {
-    className: PropTypes.string,
-    onClick: PropTypes.func,
-    children: PropTypes.any,
-    type: PropTypes.string,
-  };
+Dialog.propTypes = {
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+  children: PropTypes.any,
+  type: PropTypes.string,
+};
 
-  static defaultProps = {
-    className: 'btn',
-    onClick: () => console.log('button'),
-    children: null,
-    type: 'button',
-  };
+Dialog.defaultProps = {
+  className: 'btn',
+  onClick: () => console.log('button'),
+  children: null,
+  type: 'button',
+};
 
-  render () {
-    const { className, onClick, children, type } = this.props;
-
-    return (
-      <button
-        className={styles[className]}
-        onClick={onClick}
-        type={type}
-      >
-        {children}
-      </button>
-    );
-  }
-}
+export default Dialog;
